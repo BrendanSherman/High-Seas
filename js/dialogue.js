74e4b1c61db37fe600c892f91bdc50ac0ac96d59
natjef <natjef20@bergen.org>
2018-01-24 09:53:54 -0500

    scrubscrubscrub

--- js/dialogue.js
+++
@@ -1,8 +0,0 @@
-var tutorialText = ["Welcome to Shermanunknowns Pirategrounds!",
-    "In this game, you are a pirate captain, seeking fame and fortune on the high seas!",
-    "To move forward, press W.",
-    "Great job!",
-    "To turn to the left or right, press A or D!",
-    "Good going!",
-    "To slow down, press S."
-];
